db15919b9db5ca13112bc93032f11841ed3a8c3a
Augusto Nascimento <augustomn91@hotmail.com>
2022-10-05 23:09:17 -0300

    adicionando a estrutura da tarefa via javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,28 @@ const addInput = document.querySelector("#add-task");
 const addButton = document.querySelector(".button-add");
 const taskList = document.querySelector(".list-task");
 
+
+
+// funções
+
+const creatingTaskStructure = elem => {
+
+    const task = document.createElement('div');
+    task.classList.add('task');
+    
+    const p = document.createElement('p');
+    p.innerHTML = elem.name;
+    task.appendChild(p);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('onclick', `deleteTask(${elem.id})`);
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    task.appendChild(deleteButton);
+
+    taskList.append(task);
+};
+
+// eventos
+
+
+// inicializador
